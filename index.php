3ab9c0c594fa374fb670f1927313b99cd892ee70
vikanica <32392416+vikanica@users.noreply.github.com>
2017-11-17 17:01:56 +0200

    ryj

--- index.php
+++ index.php
@@ -24,3 +24,24 @@ class cat extends \atk4\data\Model {
     $this->addField('price',['type'=>'money']);
   }
 }
+$db = new
+\atk4\data\Persistence_SQL('mysql:dbname=friends;host=localhost','root');
+class Friends extends \atk4\data\Model {
+	public $table = 'friends';
+	function init() {
+		parent::init();
+		$this->addField('name');
+		$this->addField('surname');
+		$this->addField('phone_number',['default'=>'+371']);
+		$this->addField('nickname');
+		$this->addField('password',['type'=>'password']);
+		$this->addField('birthdate',['type'=>'date']);
+		$this->addField('notes',['type'=>'text']);
+	}
+}
+$form = $app->layout->('Form');
+$form->setModel(new Friend($db));
+$form->onSubmit(funktion($form) {
+$form->model->save();
+return $form->success('Record updated');
+});
