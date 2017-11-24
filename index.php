ef6df5943b4a42b3cf67136794c39d55fd2cb1f2
vikanica <32392416+vikanica@users.noreply.github.com>
2017-11-24 17:27:17 +0200

    zdth

--- index.php
+++ index.php
@@ -3,11 +3,11 @@ require 'vendor/autoload.php';
 
 use \atk4\ui\Header;
 
-$app = new \atk4\ui\App('Добро пожаловать в игру!');
+$app = new \atk4\ui\App('Добро пожаловать в таблицу!');
 $app->initLayout('Centered');
 
-$db = new
-\atk4\data\Persistence_SQL('mysql:dbname=cats;host=127.0.0.1','root','root');
+/*$db = new
+\atk4\data\Persistence_SQL('mysql:dbname=cats;host=localhost','root','');
 class cat extends \atk4\data\Model {
 
 	public $table = 'cat';
@@ -23,25 +23,32 @@ class cat extends \atk4\data\Model {
   	$this->addField('date',['type'=>'date']);
     $this->addField('price',['type'=>'money']);
   }
-}
+} */
 $db = new
-\atk4\data\Persistence_SQL('mysql:dbname=friends;host=localhost','root');
+\atk4\data\Persistence_SQL('mysql:dbname=friends;host=localhost','root','');
 class Friends extends \atk4\data\Model {
-	public $table = 'friends';
-	function init() {
-		parent::init();
-		$this->addField('name');
-		$this->addField('surname');
-		$this->addField('phone_number',['default'=>'+371']);
-		$this->addField('nickname');
-		$this->addField('password',['type'=>'password']);
-		$this->addField('birthdate',['type'=>'date']);
-		$this->addField('notes',['type'=>'text']);
-	}
+  	public $table = 'friends';
+function init() {
+  	parent::init();
+  	$this->addField('name');
+    $this->addField('surname');
+    $this->addField('phone_number',['default'=>371]);
+    $this->addField('email');
+  	$this->addField('password',['type'=>'password']);
+    $this->addField('birthday',['type'=>'date']);
+    $this->addField('notes', ['type'=>'text']);
 }
-$form = $app->layout->('Form');
-$form->setModel(new Friend($db));
-$form->onSubmit(funktion($form) {
-$form->model->save();
-return $form->success('Record updated');
+}
+$form = $app->layout->add('Form'); // создание формы
+$form->setModel(new Friends($db)); // подключение формы к модели и соединение с базой данных
+
+$form->onSubmit(function($form) { // функция срабатывающая при нажатии кнопки SAVE
+	$form->model->save(); // запись всех данных в нужную таблицу
+	return $form->success('Record updated'); // информация, что всё удачно записалось
+
 });
+
+$button = $app->layout->add(['Label', 'Хочу новое обновление!']);
+$crud = $app->layout->add('CRUD');
+$crud->setModel(new Friends($db));
+$crud->addQuickSearch(['name','surname']);
