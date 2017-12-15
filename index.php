8f8aeba05b3a04ed95f657a3271e8766028e4860
vikanica <32392416+vikanica@users.noreply.github.com>
2017-12-15 17:14:42 +0200

    dfhj

--- index.php
+++ index.php
@@ -7,11 +7,12 @@ $app = new \atk4\ui\App('Добро пожаловать в таблицу!');
 $app->initLayout('Centered');
 session_start();
 if(isset($_SESSION['a'])){
-$_SESSION['n']=1;
+  $m=$_SESSION['a']+$_SESSION['n'];
+  $_SESSION['a']=$_SESSION['n'];
+  $_SESSION['n']=$m;
 }else{
-$m=$_SESSION['a']+$_SESSION['n'];
-$_SESSION['a']=$_SESSION['n'];
-$_SESSION['n']=$m;
+  $_SESSION['n']=1;
+
 }
 
 
@@ -19,5 +20,5 @@ $_SESSION['n']=$m;
 $label = $app->layout->add(['Label', $m);
 $button_inc = $app->add(['Button','+']);
 $button_inc->link(['index']);
-$button_res = $app->add(['Button','+']);
+$button_res = $app->add(['Button','restart']);
 $button_res->link(['index']);
