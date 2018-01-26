5fc46b4179c2bc9b94603812eceb4a3580ce44c1
vikanica <32392416+vikanica@users.noreply.github.com>
2018-01-26 17:35:32 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,24 +1,9 @@
 <?php
-require 'connection.php';
-$app = new \atk4\ui\App('Добро пожаловать в АД! А если застрелитесь, я вас не остановлю.');
+require 'vendor/autoload.php';
+$app = new \atk4\ui\App('Какая То Теорема');
 
 $app->initLayout('Centered');
-$form = $app->layout->add('Form');
-
-$form->setModel(new Friend($db));
-$form->onSubmit(function($form) {
-
-	$form->model->save();
-
-  return new \atk4\ui\jsExpression('document.location = "index.php" ');
-
-});
-$app->add(['ui'=>'divider']);
-$crud = $app->layout->add('Grid');
-
-$crud->setModel(new Friend($db));
-
-$crud->addQuickSearch(['name','surname','phone number','age']);
-
-$button = $app->layout->add(['Button', "admin",'white inverted','iconRight'=>'coffee']);
-$button->link(['check']);
+//$form = $app->layout->add('Form');
+$check = new \atk4\data\Model(new \atk4\data\Persistence_Array($a));
+$check->addField('a');
+$check->addField('b');
