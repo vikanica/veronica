034e4d4b7277420404364155875e3296c7e65164
vikanica <32392416+vikanica@users.noreply.github.com>
2018-03-02 17:30:35 +0200

    xfg

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-$app = new \atk4\ui\App('Какая То Теорема');
-
-$app->initLayout('Centered');
-//$form = $app->layout->add('Form');
-$check = new \atk4\data\Model(new \atk4\data\Persistence_Array($a));
-$check->addField('a');
-$check->addField('b');
