6eb2e780cd7b4108685a49b347f043a3844c5019
vikanica <32392416+vikanica@users.noreply.github.com>
2018-04-06 16:11:30 +0300

    cvg

--- index.php
+++ index.php
@@ -5,5 +5,5 @@ $app = new \atk4\ui\App('Registration');
 $app->initLayout('Centered');
 $button = $app->add(['Button','click']);
 $button->icon = 'book';
-$a=$_ENV['KEY'];
-$app->add(['Label',$a]);
+//$a=$_ENV['KEY'];
+//$app->add(['Label',$a]);
